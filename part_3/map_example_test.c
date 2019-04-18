9d8a3778ead4a8718757d9e70beb0cdcadd2d67c
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2019-04-18 16:46:50 +0300

    Add map test

--- part_3/map_example_test.c
+++ part_3/map_example_test.c
@@ -94,28 +94,27 @@ bool testMapGet() {
 bool testIterator() {
     Map map = mapCreate(copyDataChar, copyKeyInt, freeChar, freeInt,
                         compareInts);
-    for (int i = 1; i < 40; ++i) {
+    for (int i = 1; i < 400; ++i) {
         char j = (char) i;
         ++j;
         ASSERT_TEST(mapPut(map, &i, &j) == MAP_SUCCESS);
     }
 
-    for (int i = 80; i >= 40; --i) {
+    for (int i = 800; i >= 400; --i) {
         char j = (char) i;
         ++j;
         ASSERT_TEST(mapPut(map, &i, &j) == MAP_SUCCESS);
     }
 
-    for (int i = 81; i < 100; ++i) {
+    for (int i = 801; i < 1000; ++i) {
         char j = (char) i;
         ++j;
         ASSERT_TEST(mapPut(map, &i, &j) == MAP_SUCCESS);
     }
-
 
     int i = 1;
     MAP_FOREACH(int *, iter, map) {
-        ASSERT_TEST(*iter == i);
+      ASSERT_TEST(*iter == i);
       i++;
     }
 
@@ -123,10 +122,237 @@ bool testIterator() {
     return true;
 }
 
+
+
+
+bool improveTestMapCreateDestroy() {
+    Map map = mapCreate(copyDataChar, copyKeyInt, freeChar, freeInt,
+                        compareInts);
+
+    ASSERT_TEST(map != NULL);
+    ASSERT_TEST(mapGetSize(map) == 0);
+    ASSERT_TEST(mapGetFirst(map) == NULL);
+    ASSERT_TEST(mapGetNext(map) == NULL);
+    mapDestroy(map);
+    return true;
+
+}
+
+bool improveTestMapAddAndSize() {
+    Map map = mapCreate(copyDataChar, copyKeyInt, freeChar, freeInt,
+                        compareInts);
+    for (int i = 1; i < 1000; ++i) {
+        char j = (char) i;
+        ++j;
+        ASSERT_TEST(mapPut(map, &i, &j) == MAP_SUCCESS);
+        ASSERT_TEST(mapPut(map, NULL, &j) == MAP_NULL_ARGUMENT);
+        ASSERT_TEST(mapPut(NULL, &i, &j) == MAP_NULL_ARGUMENT);
+        ASSERT_TEST(mapGetSize(map) == i);
+
+    }
+
+
+    ASSERT_TEST(*(int*)mapGetFirst(map) == 1);
+    ASSERT_TEST(*(int*)mapGetNext(map) == 2);
+    int k=1;
+    char j = (char) k;
+    ASSERT_TEST(mapPut(map, &k, &j) == MAP_SUCCESS);
+     k=-1;
+     j = (char) k;
+    ASSERT_TEST(mapPut(map, &k, &j) == MAP_SUCCESS);
+    ASSERT_TEST(*(int*)mapGetFirst(map) == -1);
+    k=0;
+    j = (char) k;
+    ASSERT_TEST(mapPut(map, &k, &j) == MAP_SUCCESS);
+    ASSERT_TEST(*(int*)mapGetFirst(map) == -1);
+    ASSERT_TEST(*(int*)mapGetNext(map) == 0);
+    mapDestroy(map);
+    return true;
+}
+
+bool testMapRemove() {
+    Map map = mapCreate(copyDataChar, copyKeyInt, freeChar, freeInt,
+                        compareInts);
+    for (int i = 1; i < 1000; ++i) {
+        char j = (char) i;
+        ++j;
+        ASSERT_TEST(mapPut(map, &i, &j) == MAP_SUCCESS);
+        ASSERT_TEST(mapPut(map, NULL, &j) == MAP_NULL_ARGUMENT);
+        ASSERT_TEST(mapPut(NULL, &i, &j) == MAP_NULL_ARGUMENT);
+        ASSERT_TEST(mapGetSize(map) == i);
+
+    }
+
+    ASSERT_TEST(*(int*)mapGetFirst(map) == 1);
+    ASSERT_TEST(*(int*)mapGetNext(map) == 2);
+
+    int k=1;
+    ASSERT_TEST(mapRemove(map, NULL) == MAP_NULL_ARGUMENT);
+    ASSERT_TEST(mapRemove(NULL, &k) == MAP_NULL_ARGUMENT);
+
+
+    char j = (char) k;
+    ASSERT_TEST(mapRemove(map, &k) == MAP_SUCCESS);
+    ASSERT_TEST(mapRemove(map, &k) == MAP_ITEM_DOES_NOT_EXIST);
+    ASSERT_TEST(mapPut(map, &k, &j) == MAP_SUCCESS);
+
+    k=5;
+    ASSERT_TEST(mapContains(map, &k)== true);
+    ASSERT_TEST(mapRemove(map, &k) == MAP_SUCCESS);
+    ASSERT_TEST(mapContains(map, &k)== false);
+    ASSERT_TEST(mapPut(map, &k, &j) == MAP_SUCCESS);
+    ASSERT_TEST(mapContains(map, &k)== true);
+
+
+    mapDestroy(map);
+    return true;
+}
+
+bool testMapCopy() {
+    Map map = mapCreate(copyDataChar, copyKeyInt, freeChar, freeInt,
+                        compareInts);
+    for (int i = 1; i < 1000; ++i) {
+        char j = (char) i;
+        ++j;
+        ASSERT_TEST(mapPut(map, &i, &j) == MAP_SUCCESS);
+        ASSERT_TEST(mapPut(map, NULL, &j) == MAP_NULL_ARGUMENT);
+        ASSERT_TEST(mapPut(NULL, &i, &j) == MAP_NULL_ARGUMENT);
+        ASSERT_TEST(mapGetSize(map) == i);
+
+    }
+    Map map2 = mapCopy(map);
+
+
+    ASSERT_TEST(*(int*)mapGetFirst(map2) == 1);
+    ASSERT_TEST(*(int*)mapGetNext(map2) == 2);
+    int k=1;
+    char j = (char) k;
+    ASSERT_TEST(mapPut(map2, &k, &j) == MAP_SUCCESS);
+    k=-1;
+    j = (char) k;
+    ASSERT_TEST(mapPut(map2, &k, &j) == MAP_SUCCESS);
+    ASSERT_TEST(*(int*)mapGetFirst(map2) == -1);
+    k=0;
+    j = (char) k;
+    ASSERT_TEST(mapPut(map2, &k, &j) == MAP_SUCCESS);
+    ASSERT_TEST(*(int*)mapGetFirst(map2) == -1);
+    ASSERT_TEST(*(int*)mapGetNext(map2) == 0);
+    mapDestroy(map);
+    mapDestroy(map2);
+    return true;
+}
+
+bool testMapContains() {
+    Map map = mapCreate(copyDataChar, copyKeyInt, freeChar, freeInt,
+                        compareInts);
+    for (int i = 1; i < 1000; ++i) {
+        char j = (char) i;
+        ++j;
+        ASSERT_TEST(mapPut(map, &i, &j) == MAP_SUCCESS);
+        ASSERT_TEST(mapPut(map, NULL, &j) == MAP_NULL_ARGUMENT);
+        ASSERT_TEST(mapPut(NULL, &i, &j) == MAP_NULL_ARGUMENT);
+        ASSERT_TEST(mapGetSize(map) == i);
+
+    }
+    Map map2 = mapCopy(map);
+
+    ASSERT_TEST(*(int*)mapGetFirst(map2) == 1);
+    ASSERT_TEST(*(int*)mapGetNext(map2) == 2);
+    int k=1;
+    char j = (char) k;
+    ASSERT_TEST(mapContains(map, &k)== true);
+    k=-1;
+    j = (char) k;
+    ASSERT_TEST(mapPut(map2, &k, &j) == MAP_SUCCESS);
+    ASSERT_TEST(*(int*)mapGetFirst(map2) == -1);
+    k=0;
+    j = (char) k;
+    ASSERT_TEST(mapPut(map2, &k, &j) == MAP_SUCCESS);
+    ASSERT_TEST(*(int*)mapGetFirst(map2) == -1);
+    ASSERT_TEST(*(int*)mapGetNext(map2) == 0);
+    k=999;
+    ASSERT_TEST(mapContains(map2, &k)== true);
+    k=-1;
+    ASSERT_TEST(mapContains(map2, &k)== true);
+    k=20;
+    ASSERT_TEST(mapContains(map2, &k)== true);
+    k=20000;
+    ASSERT_TEST(mapContains(map2, &k)== false);
+    mapDestroy(map);
+    mapDestroy(map2);
+    return true;
+}
+
+bool improveTestMapGet() {
+    Map map = mapCreate(copyDataChar, copyKeyInt, freeChar, freeInt,
+                        compareInts);
+    for (int i = 1; i < 1000; ++i) {
+        char j = (char) i;
+        ++j;
+        ASSERT_TEST(mapPut(map, &i, &j) == MAP_SUCCESS);
+        ASSERT_TEST(mapGetSize(map) == i);
+    }
+
+    for (int i = 1; i < 1000; ++i) {
+        char j = (char) i;
+        ++j;
+        char *getVal = (char *) mapGet(map, &i);
+        ASSERT_TEST(*getVal == j);
+    }
+    int i = 0;
+    ASSERT_TEST(mapGet(map, &i) == NULL);
+    i = 1000;
+    ASSERT_TEST(mapGet(map, &i) == NULL);
+    mapDestroy(map);
+    return true;
+}
+
+bool improveTestIterator() {
+    Map map = mapCreate(copyDataChar, copyKeyInt, freeChar, freeInt,
+                        compareInts);
+    for (int i = 1; i < 400; ++i) {
+        char j = (char) i;
+        ++j;
+        ASSERT_TEST(mapPut(map, &i, &j) == MAP_SUCCESS);
+    }
+
+    for (int i = 800; i >= 400; --i) {
+        char j = (char) i;
+        ++j;
+        ASSERT_TEST(mapPut(map, &i, &j) == MAP_SUCCESS);
+    }
+
+    for (int i = 801; i < 1000; ++i) {
+        char j = (char) i;
+        ++j;
+        ASSERT_TEST(mapPut(map, &i, &j) == MAP_SUCCESS);
+    }
+
+    int i = 1;
+    MAP_FOREACH(int *, iter, map) {
+        ASSERT_TEST(*iter == i);
+        i++;
+    }
+
+    mapDestroy(map);
+    return true;
+}
+
+
+
+
 int main() {
     RUN_TEST(testMapCreateDestroy);
     RUN_TEST(testMapAddAndSize);
     RUN_TEST(testMapGet);
     RUN_TEST(testIterator);
+    RUN_TEST(improveTestMapCreateDestroy);
+    RUN_TEST(improveTestMapAddAndSize);
+    RUN_TEST(improveTestMapGet);
+    RUN_TEST(improveTestIterator);
+    RUN_TEST(testMapCopy);
+    RUN_TEST(testMapContains);
+    RUN_TEST(testMapRemove);
+
     return 0;
 }
