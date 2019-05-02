86bf411f98c560af2092495c3e7956f681b88444
DanielShkolnik <DanielShkolnik@users.noreply.github.com>
2019-05-02 16:00:56 +0300

    Part_2 Cmake create

--- part_1/main.c
+++ part_1/main.c
@@ -17,18 +17,18 @@ int main() {
 }
 
 //Convention Error 3: Lack of spaces
-char *stringDuplicator(const char *s, int times) { // Convention Error 1 + 2: Use of Const in function and wrong variable name s instead of str
- assert(s); // Compilation Error 1:  Wrong use of !
+char *duplicateString(char *s, int times) {
+ assert(s);
  assert(times > 0);
- int LEN = strlen(s); // Convention Error 3: variable name with capital letters
- char *out = malloc(sizeof(char)*LEN*times); // Convention Error 3: sizeof(char) in malloc
- if (out==NULL){ // Compilation Error 2: Return Null in case of fail to malloc
+ int len = strlen(s);
+ char *out = malloc(len*times);
+ if (out==NULL){
      return NULL;
  }
- assert(out); // Replaced with IF
+ assert(out);
  for (int i = 0; i < times; i++) {
-     strcpy(out, s); // Compilation Error 3: Change the order of strcpy & out commands
-     out = out + LEN;
+     strcpy(out, s);
+     out = out + len;
  }
- return out-times*LEN; // Compilation Error 4: Return the finish of the string instead of the beginning
+ return out-times*len;
 }
