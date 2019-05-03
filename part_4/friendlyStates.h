5d9d699ab4429cd628c5b1426c94696878570cd5
DanielShkolnik <danielshkolnik@users.noreply.github.com>
2019-05-03 17:13:10 +0300

    add comments

--- part_4/friendlyStates.h
+++ part_4/friendlyStates.h
@@ -7,8 +7,10 @@
 #include <stdbool.h>
 #include <string.h>
 
+/** Type for defining the FriendlyStates struct */
 typedef struct FriendlyStates_t *FriendlyStates;
 
+/** Type used for returning error codes from FriendlyStates functions */
 typedef enum {
     FRIENDLYSTATES_SUCCESS,
     FRIENDLYSTATES_OUT_OF_MEMORY,
@@ -17,11 +19,66 @@ typedef enum {
     FRIENDLYSTATES_ITEM_DOES_NOT_EXIST
 } FriendlyStatesResult;
 
+/**
+ * Allocates a new FriendlyStates.
+ * Create FriendlyStates with state1 name (string) and state2 name (string)
+ *
+ * @param nameState1 = state1 name
+ * @param nameState2 = state2 name
+ * @return new FriendlyStates pointer with the params inserted
+ */
 FriendlyStates friendlyStatesCreate(const char* nameState1, const char* nameState2);
+
+/**
+ * friendlyStatesDestroy: Deallocates an existing Judge.
+ * Clears all elements.
+ *
+ * @param friendlyStates Target FriendlyStates to be deallocated. If list is NULL nothing will be
+ * done.
+ *
+ * @return
+ * FRIENDLYSTATES_NULL_ARGUMENT if Judge pointer is NULL
+ * FRIENDLYSTATES_SUCCESS if the Judge destroyed successfully.
+ */
 FriendlyStatesResult friendlyStatesDestroy(FriendlyStates friendlyStates);
+
+/**
+ * Compare 2 FriendlyStates by their lexicographical order name.
+ *
+ * @param friendlyStates1 to compare
+ * @param friendlyStates2 to compare
+ * @return int number that is negative if friendlyStates2 bigger than friendlyStates1, equals zero
+ * if the 2 friendlyStates name is the same or positive if friendlyStates1 bigger than friendlyStates2.
+ */
 int friendlyStatesCompare(FriendlyStates friendlyStates1, FriendlyStates friendlyStates2);
+
+/**
+*Creates a copy of target friendlyStates.
+*
+* The new copy will contain all the elements from the source judge in the same order
+*
+* @param friendlyStates The target friendlyStates to copy
+* @return
+* NULL if a NULL was sent or a memory allocation failed.
+* A FriendlyStates containing the same elements with same order.
+*
+ */
 FriendlyStates friendlyStatesCopy(FriendlyStates friendlyStates);
+
+/**
+ * Returns the friendlyStates state 1 name.
+ *
+ * @param friendlyStates The FriendlyStates for which to get the name of the first state
+ * @return The friendlyStates name of state 1 from the FriendlyStates inserted
+ */
 char* friendlyStatesGetName1(FriendlyStates friendlyStates);
+
+/**
+ * Returns the friendlyStates state 2 name.
+ *
+ * @param friendlyStates The FriendlyStates for which to get the name of the second state
+ * @return The friendlyStates name of state 2 from the FriendlyStates inserted
+ */
 char* friendlyStatesGetName2(FriendlyStates friendlyStates);
 
 
--- part_4/stateScore.h
+++ part_4/stateScore.h
@@ -7,8 +7,10 @@
 #include <stdbool.h>
 #include <string.h>
 
+/** Type for defining the StateScore struct */
 typedef struct StateScore_t *StateScore;
 
+/** Type used for returning error codes from StateScore functions */
 typedef enum {
     STATESCORE_SUCCESS,
     STATESCORE_OUT_OF_MEMORY,
@@ -17,10 +19,63 @@ typedef enum {
     STATESCORE_ITEM_DOES_NOT_EXIST
 } StateScoreResult;
 
+
+/**
+ * Allocates a new StateScore.
+ * Create StateScore with state id number (int), state name (string), score average by other states (double)
+ * judge score average (double) and percent of how to calculate state scores and judge scores (int).
+ *
+ * @param id = id number of the state
+ * @param name = name of the state
+ * @param stateVotesAverage = score average by other states
+ * @param judgesVotesAverage = judge score average
+ * @param audiencePercent = percent of how to calculate state scores and judge scores
+ * @return new StateScore pointer with the params inserted
+ */
 StateScore stateScoreCreate(int id, char* name, double stateVotesAverage, double judgesVotesAverage, int audiencePercent);
+
+/**
+ * stateScoreDestroy: Deallocates an existing StateScore.
+ * Clears all elements.
+ *
+ * @param stateScore Target StateScore to be deallocated. If list is NULL nothing will be
+ * done.
+ *
+ * @return
+ * STATESCORE_NULL_ARGUMENT if StateScore pointer is NULL
+ * STATESCORE_SUCCESS if the StateScore destroyed successfully.
+ */
 StateScoreResult stateScoreDestroy(StateScore stateScore);
+
+/**
+ * Compare 2 StateScore by their id number.
+ *
+ * @param stateScore1 to compare
+ * @param stateScore2 to compare
+ * @return int number that is negative if stateScore2 bigger than stateScore1, equals zero
+ * if the 2 StateScore id is the same or positive if stateScore1 bigger than stateScore2.
+ */
 int stateScoreCompare(StateScore stateScore1, StateScore stateScore2);
+
+/**
+*Creates a copy of target StateScore.
+*
+* The new copy will contain all the elements from the source StateScore in the same order
+*
+* @param stateScore The target stateScore to copy
+* @return
+* NULL if a NULL was sent or a memory allocation failed.
+* A StateScore containing the same elements with same order.
+*
+ */
 StateScore stateScoreCopy(StateScore stateScore);
+
+/**
+ * Returns the stateScore name.
+ *
+ * @param stateScore The StateScore for which to get the name
+ * @return The stateScore name from the StateScore inserted
+ */
 char* stateScoreGetName(StateScore stateScore);
 
 
