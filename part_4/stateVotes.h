13eddcb684fe0f5db44f945d81936368879aef0e
OmerHemo <OmerHemo@users.noreply.github.com>
2019-05-03 16:12:20 +0300

    comments

--- part_4/stateVotes.h
+++ part_4/stateVotes.h
@@ -13,14 +13,59 @@ typedef enum {
     STATEVOTE_ITEM_ALREADY_EXISTS,
     STATEVOTE_ITEM_DOES_NOT_EXIST
 } StatVoteResult;
+/**
+ * This struct purpose is to store all the votes to a state for a state.
+ * So this struct comes in use in the state struct.
+ */
 typedef struct StateVote_t *StateVote;
-
+/**
+ * creates a state vote.
+ * @param stateId: the id of a state to give votes to.
+ * @return a new state vote.
+ */
 StateVote stateVoteCreate(int stateId);
+/**
+ * Creates a new copy of state vote.
+ * @param stateVote
+ * @return a new copy of state vote if succedded or null if an error occurred.
+ */
 StateVote stateVoteCopy(StateVote stateVote);
+/**
+ * Destry a state vote and free all its alocated data in the heap.
+ * @param stateVote too be destroyed
+ */
 void stateVoteDestroy(StateVote stateVote);
+/**
+ * Compares state votes by their ids.
+ * @param stateVote1
+ * @param stateVote2
+ * @return positive if stateVote1 id > stateVte2 id.
+ *         negative if stateVte1 id < stateVote2 id.
+ *         0 if equal.
+ */
 int stateVoteCompare(StateVote stateVote1,StateVote stateVote2);
+/**
+ * Adds a vote to a stateVote.
+ * @param vote : voted stateVote
+ * @return Success if no error else the error which occurred
+ */
 StatVoteResult stateVoteAddVote(StateVote vote);
+/**
+ * Removes a vote to a stateVote.
+ * @param vote : voted stateVote
+ * @return Success if no error else the error which occurred
+ */
 StatVoteResult stateVoteRemoveVote(StateVote vote);
+/**
+ * Gets the state id of a stateVote.
+ * @param vote : the StateVote
+ * @return the id of a stateVote.
+ */
 int stateVoteGetId(StateVote vote);
+/**
+ * returns the number of votes a state has
+ * @param vote the stateVote
+ * @return the number of votes a state has
+ */
 int stateVoteGetVote(StateVote vote);
 #endif //PART_4_STATEVOTES_H
